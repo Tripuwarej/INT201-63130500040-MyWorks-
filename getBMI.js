be8d6a909301701b5f4e5fbf86891f747b85fd93
Tripuwarej <68810388+Tripuwarej@users.noreply.github.com>
2021-09-05 01:28:16 +0700

    Add files via upload

---
+++ getBMI.js
@@ -0,0 +1,5 @@
+// หาค่า BMI
+function getBMI(weigth, height){
+    return weigth/(height*height) 
+}
+console.log(getBMI(85,164/100).toFixed(2)) //น้ำหนักตัวเป็นกิโลกรัมและหารด้วยส่วนสูงที่วัดเป็นเมตรยกกำลังสอง
