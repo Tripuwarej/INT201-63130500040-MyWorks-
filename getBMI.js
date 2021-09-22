4f3d4fddc67b6424ff78bb3e263f175f122432e2
Tripuwarej <68810388+Tripuwarej@users.noreply.github.com>
2021-09-22 17:47:32 +0700

    Rename getBMI.js to Ex.3-getBMI.js

--- getBMI.js
+++
@@ -1,5 +0,0 @@
-// หาค่า BMI
-function getBMI(weigth, height){
-    return weigth/(height*height) 
-}
-console.log(getBMI(85,164/100).toFixed(2)) //น้ำหนักตัวเป็นกิโลกรัมและหารด้วยส่วนสูงที่วัดเป็นเมตรยกกำลังสอง
